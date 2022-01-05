e6c9a46b826d72a909b4e947afda3ed288f62380
Hanna Lee <8annahxxl@gmail.com>
2022-01-05 12:43:14 +0900

    add: 실습에 필요한 더미 데이터 추가

---
+++ src/ch01/data/invoices.js
@@ -0,0 +1,19 @@
+export const invoices = [
+  {
+    customer: "BigCo",
+    performances: [
+      {
+        playId: "hamlet",
+        audience: 55,
+      },
+      {
+        playId: "asLike",
+        audience: 35,
+      },
+      {
+        playId: "othello",
+        audience: 40,
+      },
+    ],
+  },
+];
---
+++ src/ch01/data/plays.js
@@ -0,0 +1,14 @@
+export const plays = {
+  hamlet: {
+    name: "Hamlet",
+    type: "tragedy",
+  },
+  asLike: {
+    name: "As you Like It",
+    type: "comedy",
+  },
+  othello: {
+    name: "Othello",
+    type: "tragedy",
+  },
+};
