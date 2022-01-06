55d195f1ed363c2fc51b85b27023c47738f4976d
Hanna Lee <8annahxxl@gmail.com>
2022-01-07 07:15:01 +0900

    test: statement 테스트 수정

--- src/ch01/test/statement.test.js
+++ src/ch01/test/statement.test.js
@@ -29,7 +29,7 @@ describe("statement", () => {
     };
   });
 
-  it("청구 내역을 출력한다.", () => {
+  it("statementData", () => {
     const result = statement(invoice, plays);
     expect(result).toBe(
       "청구 내역 (고객명: BigCo)\n  Hamlet: $650.00 (55석)\n  As You Like It: $580.00 (35석)\n  Othello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47점\n"
