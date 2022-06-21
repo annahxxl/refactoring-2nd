59ee987050e068b76130ec8b56ae92889878ccb2
Hanna Lee <8annahxxl@gmail.com>
2022-06-21 21:03:07 +0900

    6.10 여러 함수를 변환 함수로 묶기

--- practice/ch6/6-10.js
+++ practice/ch6/6-10.js
@@ -1,7 +1,20 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+import _ from "lodash";
+
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+export function enrichReading(original) {
+  // const result = {...original}; // Spread & Object.assign: 얕은 복사
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, result.baseCharge - 0.1);
+  return result;
+}
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
 }
 
 export function baseRate(month, year) {
