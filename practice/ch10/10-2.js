ca875e1427d8c766da9b281757ea2f5a95696cfb
Hanna Lee <8annahxxl@gmail.com>
2022-07-12 05:30:40 +0900

    10.2 조건식 통합하기

--- practice/ch10/10-2.js
+++ practice/ch10/10-2.js
@@ -1,6 +1,14 @@
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  if (isNotEligibleFordisability(employee)) {
+    return 0;
+  }
   return 1;
+}
+
+function isNotEligibleFordisability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
 }
