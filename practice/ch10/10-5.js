d56a6a22ae22b85a7468cf1ad0c5ea76a304d782
Hanna Lee <8annahxxl@gmail.com>
2022-07-14 06:59:24 +0900

    10.5 특이 케이스 추가하기

--- practice/ch10/10-5.js
+++ practice/ch10/10-5.js
@@ -1,10 +1,55 @@
+// 예제 2
+class Hotel {
+  constructor() {
+    this.rooms = [];
+  }
+
+  addRoom(roomNumber) {
+    this.rooms[roomNumber] = new Room(roomNumber);
+  }
+
+  emptyRoom(roomNumber) {
+    this.rooms[roomNumber] = new EmptyRoom(roomNumber);
+  }
+
+  cleanRooms() {
+    this.rooms.forEach((room) => room.clean());
+  }
+}
+
+class Room {
+  constructor(roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  clean() {
+    console.log(`${this.roomNumber} 깨끗하게 청소합니다!`);
+  }
+}
+
+class EmptyRoom extends Room {
+  clean() {
+    console.log(`${this.roomNumber} 방이 비어있어요~~`);
+  }
+}
+
+const hotel = new Hotel();
+hotel.addRoom(0);
+hotel.addRoom(1);
+hotel.cleanRooms();
+hotel.emptyRoom(0);
+hotel.cleanRooms();
+
+// 예제 1
 export class Site {
   constructor(customer) {
     this._customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === "unknown"
+      ? new UnknownCustomer()
+      : new Customer(this._customer);
   }
 }
 
@@ -30,13 +75,16 @@ export class Customer {
   }
 }
 
+class UnknownCustomer extends Customer {
+  get name() {
+    return "occupant";
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
   // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
-
+  const customerName = aCustomer.name;
   return customerName;
 }
