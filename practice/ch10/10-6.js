8d8b52c3202079f10a62350407cf024bfb2d6649
Hanna Lee <8annahxxl@gmail.com>
2022-07-14 07:04:19 +0900

    10.6 어서션 추가하기

--- practice/ch10/10-6.js
+++ practice/ch10/10-6.js
@@ -1,8 +1,13 @@
+import { strict as assert } from "node:assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    assert(number >= 0); // 개발 단계에서만 사용, 배포 환경에서는 버그 리포트 or 기본값 설정
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+new Customer().applyDiscount(-1);
